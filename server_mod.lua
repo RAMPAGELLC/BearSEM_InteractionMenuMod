0a06816ae683b92561e8b915ce0db35b726cb10e
vq9o <53842001+vq9o@users.noreply.github.com>
2025-01-08 23:59:57 -0800

    Update server_mod.lua
    Production working.

--- server_mod.lua
+++ server_mod.lua
@@ -1,39 +1,36 @@
 local departmentPerms = {
-	-- [department] = {perms}
-	["NONE"] = {
-		-- Default/required to remove all perms when going off-duty.
+	["DEFAULT"] = {
+		["LEO"] = false,
+		["Fire"] = false,
 
-		LEO = false,
-		Fire = false,
-
-		Unjail = false,
-		Unhospital = false,
+		["Unjail"] = false,
+		["Unhospital"] = false
 	},
 
 	["SAFR"] = {
-		LEO = false,
-		Fire = true,
+		["LEO"] = false,
+		["Fire"] = true,
 
-		Unjail = false,
-		Unhospital = false,
+		["Unjail"] = false,
+		["Unhospital"] = true
 	},
 
 	["BCSO"] = {
-		LEO = true,
-		Fire = false,
+		["LEO"] = true,
+		["Fire"] = false,
 
-		Unjail = true,
-		Unhospital = false,
-	},
+		["Unjail"] = true,
+		["Unhospital"] = false
+	}
 }
 
-AddEventHandler("bearduty_rich:DUTY_STATUS_CHANGE", function(source, isOnDuty, trackData)
+AddEventHandler("bearduty_rich:BearSEMInteractionMod", function(source, isOnDuty, trackData)
 	if isOnDuty and trackData.Department ~= "NONE" then
-		for i, v in pairs(departmentPerms[trackData.Department] or departmentPerms["NONE"]) do
+		for i, v in pairs(departmentPerms[trackData.Department] or departmentPerms["DEFAULT"]) do
 			TriggerClientEvent("SEM_InteractionMenu:" .. i .. "PermsResult", source, v)
 		end
 	else
-		for i, v in pairs(departmentPerms["NONE"]) do
+		for i, v in pairs(departmentPerms["DEFAULT"]) do
 			TriggerClientEvent("SEM_InteractionMenu:" .. i .. "PermsResult", source, v)
 		end
 	end
